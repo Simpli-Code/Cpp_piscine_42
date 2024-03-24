da3162363a23143831fe71c9858c096792303d75
Simpli-Code <74283859+Simpli-Code@users.noreply.github.com>
2024-03-24 17:45:35 +0100

    Modified

--- Module00/ex00/src/megaphone.cpp
+++ Module00/ex00/src/megaphone.cpp
@@ -6,38 +6,43 @@
 /*   By: chruhin <chruhin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 22:32:58 by chruhin           #+#    #+#             */
-/*   Updated: 2024/03/07 21:47:35 by chruhin          ###   ########.fr       */
+/*   Updated: 2024/03/24 17:42:53 by chruhin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 /*
-This function takes a string as input and converts it using toupper(); function
-to upper case letters and return the converted string.
+This function takes a string as input and converts it using toupper();
+function to upper case letters and return the converted string.
 */
 
-char	*convert_string(char *str)
+std::string convert_string(char *str)
 {
-	int	i = -1;
+	int	i = 0;
+	std::string new_str = str;
 
-	while (str[++i])
-		str[i] = std::toupper(str[i]);
-	return (str);
+	while (new_str[i])
+	{
+		new_str[i] = std::toupper(new_str[i]);
+		i++;
+	}
+	return (new_str);
 }
 
 int	main(int argc, char **argv)
 {
-	int	i = 0;
+	int	i = 1;
 	std::string message = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 	if (argc == 1)
 		return (std::cout << message << std::endl, 0);
 	else
 	{
-		while (++i < argc)
+		while (i < argc)
 		{
-			std::cout << convert_string(argv[i]) << ' ';
+			std::cout << convert_string(argv[i]);
+			i++;
 		}
 	}
 	std::cout << std::endl;
