52a28d3a6238acdd11b000885b505841a540bde9
Simpli-Code <74283859+Simpli-Code@users.noreply.github.com>
2024-03-30 21:21:09 +0100

    modified

--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chruhin <chruhin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 11:24:24 by chruhin           #+#    #+#             */
-/*   Updated: 2024/03/30 13:36:10 by chruhin          ###   ########.fr       */
+/*   Updated: 2024/03/30 21:20:00 by chruhin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ int	main(void)
 {
 	Harl harl;
 
-	// Test Harl's complaints
 	harl.complain(DEBUG);
 	harl.complain(INFO);
 	harl.complain(WARNING);
 	harl.complain(ERROR);
-
-	// Test invalid complaint level
 	harl.complain(INVALID);
 	return (0);
 }
